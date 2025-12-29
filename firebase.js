15e8e56fb062faa6890c07e6f9d290024881f103
tresor676 <habimanatresor676@gmail.com>
2025-12-29 10:05:35 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,9 +3,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TA_API_KEY",
-  authDomain: "TON_PROJET.firebaseapp.com",
-  projectId: "TON_PROJET",
+  apiKey: "AIzaSyC2WG-135oKY_s6xf5-nBBhxncNV9UayQ0",
+  authDomain: "tresor-ae58e.firebaseapp.com",
+  databaseURL: "https://tresor-ae58e-default-rtdb.firebaseio.com",
+  projectId: "tresor-ae58e",
+  storageBucket: "tresor-ae58e.firebasestorage.app",
+  messagingSenderId: "835562519447",
+  appId: "1:835562519447:web:b2a10371f0d71f4ac522d2"
 };
 
 export const app = initializeApp(firebaseConfig);
